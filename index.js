1e40f88d3540c759a5b3e33d4182e1020d737595
kendrixcrew-beep <kendrixcrew@gmail.com>
2026-07-22 00:57:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const config = require("./config");
 console.log("================================");
 console.log("🤖 " + config.BOT_NAME);
 console.log("👤 Owner : " + config.OWNER_NAME);
-console.log("⚡ Prefix: " + config.PREFIX)
+console.log("⚡ Prefix: " + config.PREFIX);
 console.log("📦 Version: " + config.VERSION);
 console.log("================================");
