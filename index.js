d657688153cb1fbf8c5b9c001cc3189c9ab46d4b
kendrixcrew-beep <kendrixcrew@gmail.com>
2026-07-22 00:54:57 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("🚀 Shadow MD is starting...");
+const config = require("./config");
+
+console.log("================================");
+console.log("🤖 " + config.BOT_NAME);
+console.log("👤 Owner : " + config.OWNER_NAME);
+console.log("⚡ Prefix: " + config.PREFIX);
+console.log("📦 Version: " + config.VERSION);
+console.log("================================");
