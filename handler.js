1409aa7f8155bac29838452e46e257ceba8f0572
kendrixcrew-beep <kendrixcrew@gmail.com>
2026-07-23 23:40:45 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,25 +1,25 @@
-const config = require("./config");
+const ping = require("./commands/general/ping");
+const menu = require("./commands/general/menu");
 
 function handleCommand(message) {
 
-    if (!message.startsWith(config.PREFIX)) {
+    if (!message.startsWith("!")) {
         return;
     }
 
     const command = message.slice(1).toLowerCase();
 
-    if (command === "ping") {
-        console.log("🏓 Pong!");
+    if (command === ping.name) {
+        ping.execute();
     }
 
-    else if (command === "menu") {
-        console.log("📋 Opening Menu...");
+    else if (command === menu.name) {
+        menu.execute();
     }
 
     else {
-        console.log("❌ Unknown command.");
+        console.log("❌ Unknown command");
     }
-
 }
 
 module.exports = handleCommand;
